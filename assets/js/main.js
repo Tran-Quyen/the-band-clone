58474aac68fd8d1fc48c27058a8dfec1e974eadd
Tran-Quyen <quyen10a8.sao@gmail.com>
2022-04-26 22:37:12 +0700

    Feature: Finish the band component.

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Case 1
+/*
+  data-interval="1500"
+  data-ride="true"
+  data-pause="false"
+  $('.carousel').carousel()
+*/
+
+// Case 2
+$('.carousel').carousel({
+  interval: 1500,
+  ride: true,
+  pause: false,
+});
+
+

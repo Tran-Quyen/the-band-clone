d8bfab1f5b8241f4522c7b5156e150a98e327f22
Tran-Quyen <quyen10a8.sao@gmail.com>
2022-05-08 09:15:51 +0700

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,4 +13,54 @@ $('.carousel').carousel({
   pause: false,
 });
 
+// Link: filter: hue-rotate(180deg);
+var copyright = document.querySelector('.footer-copyright > a');
+var corner = 0;
+setInterval(() => {
+  copyright.style.filter = `hue-rotate(${corner}deg)`;
+  corner += 20;
+}, 500);
+
+// Sroll smooth
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (e) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        e.preventDefault();
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(':focus')) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
+
 
